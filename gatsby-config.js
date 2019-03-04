d793855fce213634659ffa3f2172f0dbf9843e31
Brandon Dail <brandondail@fb.com>
2019-03-03 18:19:58 -0800

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `Blog`,
+    author: `Brandon Dail`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `aweary`,
     },
   },
   plugins: [
